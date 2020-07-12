8ee7012d2eb4b41623a7d7b7051b41a396491645
Gonzalo Sian <gonzalosian@gmail.com>
2020-07-11 21:17:39 -0300

    docs agregado

---
+++ docs/precache-manifest.584908588d838ae42aaa929d198abb41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360a416340a54327bd9b61f459fb2f00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0b32a6769554d7554ba",
+    "url": "/static/css/main.fbbe5221.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b32a6769554d7554ba",
+    "url": "/static/js/main.8e4d8c3c.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
